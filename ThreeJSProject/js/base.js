c1c6605f5219ec4b09eacf2357fb1c38bc772068
Leonard Faix <leonard.faix@googlemail.com>
2019-10-23 17:56:38 +0200

    Revert "FBX Zombie import."
    This reverts commit 320baa5c9b7432f57f88b79952c7783e3af70eaa.

--- ThreeJSProject/js/base.js
+++ ThreeJSProject/js/base.js
@@ -2,10 +2,7 @@ var scene = new THREE.Scene();
 scene.background = new THREE.Color(0x111111);
 
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(0,2,3);
-
-var mixer;
-var clock = new THREE.Clock();
+camera.position.z = 7;
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -18,39 +15,10 @@ window.addEventListener('resize', function () {
 });
 
 var start = function () {
-    
-    var light = new THREE.PointLight(0xffffff, 5);
-    light.position.set(0, 3, 5);
-    scene.add(light);
 
-    var light = new THREE.PointLight(0xffffff, 5);
-    light.position.set(2, 10, 0);
-    scene.add(light);
-
-
-    var loader = new THREE.FBXLoader();
-    loader.load( './zombie2.fbx', function ( object ) {
-        object.position.set(0,0,0);
-        mixer = new THREE.AnimationMixer( object );
-        var action = mixer.clipAction( object.animations[ 0 ] );
-        action.play();
-        object.traverse( function ( child ) {
-            if ( child.isMesh ) {
-                child.castShadow = true;
-                child.receiveShadow = true;
-            }
-        } );
-        scene.add( object );
-    
-    }, undefined, function ( e ) {
-    
-      console.error( e );
-    
-    } );
 }
 var update = function () {
-    var delta = clock.getDelta();
-    if ( mixer ) mixer.update( delta );
+
 };
 //Draws the scene using the camera.
 var render = function () {
